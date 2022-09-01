bc4dddc56361b9a12cb20838c45249d5e9f4731b
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2022-09-01 17:18:18 +0530

    Time: 3 ms (76.91%), Space: 60.5 MB (13.91%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -14,18 +14,40 @@
  * }
  */
 class Solution {
-    int count = 0;
+    // int count = 0;
+    // public int goodNodes(TreeNode root) {
+    //     helper(root, root.val);
+    //     return count; // returning the count value
+    //}
+    // private void helper(TreeNode root, int max) {
+    //     if (root != null) {
+    //         if (root.val >= max) { // if root.val > the max value of path from root of the tree to current node 
+    //             count++; //increment count
+    //         }
+    //         helper(root.left, Math.max(root.val, max));  // updating max value of current path and traversing left to the current node
+    //         helper(root.right, Math.max(root.val, max));  // updating max value of current path and traversing right to the current node
+    //     }
+    // }
     public int goodNodes(TreeNode root) {
-        helper(root, root.val);
-        return count; // returning the count value
+        if(root == null){ return 0; }
+        int count = 1;
+        
+        count += dfs(root.left, root.val);
+        count += dfs(root.right, root.val);
+        return count;
     }
-    private void helper(TreeNode root, int max) {
-        if (root != null) {
-            if (root.val >= max) { // if root.val > the max value of path from root of the tree to current node 
-                count++; //increment count
-            }
-            helper(root.left, Math.max(root.val, max));  // updating max value of current path and traversing left to the current node
-            helper(root.right, Math.max(root.val, max));  // updating max value of current path and traversing right to the current node
+    
+    public int dfs(TreeNode root, int currentMax){
+        
+        int amount = 0; 
+        if(root == null){ return 0; }
+        if(root.val >= currentMax){
+            amount++; 
+            currentMax = root.val;
         }
+        
+        amount += dfs(root.left, currentMax);
+        amount += dfs(root.right, currentMax);
+        return amount; 
     }
 }
