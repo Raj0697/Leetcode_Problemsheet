fa87010ef7647c0e11312bd6bfdfe538ddcab402
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2022-09-04 15:08:43 +0530

    Time: 4 ms (49.89%), Space: 41.6 MB (42.77%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.java
+++ 441-arranging-coins/441-arranging-coins.java
@@ -1,8 +1,26 @@
 class Solution {
     public int arrangeCoins(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException("Input Number is invalid. Only positive numbers are allowed");
+    //     if (n < 0) {
+    //         throw new IllegalArgumentException("Input Number is invalid. Only positive numbers are allowed");
+    //     }
+    //     return (int) (Math.sqrt(2 * (long) n + 0.25) - 0.5);
+    // }
+        
+        long s = 0, e = n;
+        while(s <= e){
+        long mid = s + (e-s)/2;
+        long k = mid*(mid+1)/2;
+        
+        if(k == n){
+            return (int)mid;
         }
-        return (int) (Math.sqrt(2 * (long) n + 0.25) - 0.5);
+        else if (k > n){
+            e = mid - 1;
+        }
+        else{
+            s = mid + 1;
+        }
     }
+    return (int)e;
+}
 }
