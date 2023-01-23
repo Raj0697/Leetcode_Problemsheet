8cd285fec9672b1de7cab940710d67c21ab97577
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2023-01-23 13:21:52 +0530

    Time: 0 ms (100.00%), Space: 38.6 MB (98.90%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,25 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n <= 0)
-            return 0;
+        //if(n <= 0)
+        //    return 0;
         if(n == 1)
             return 1;
         if(n == 2)
-            return 2;
+            return 2; /*
         int total = 0, nminus1 = 2, nminus2 = 1;
         for(int i=2; i<n; i++) {
             total = nminus1 + nminus2;
             nminus2 = nminus1;
             nminus1 = total;
         }
-        return total;
+        return total; */
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
