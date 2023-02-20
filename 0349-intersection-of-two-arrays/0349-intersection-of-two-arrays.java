5577ce3b111570d9c9ed4b0d5cca7e2072bfbed5
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2023-02-20 20:39:35 +0530

    Time: 3 ms (54.77%), Space: 42.5 MB (43.78%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -3,20 +3,20 @@ class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        // int i=0, j=0;
-        // while(i<nums1.length && j<nums2.length) {
-        //     if(nums1[i] < nums2[j]) i++;
-        //     else if(nums1[i] > nums2[j]) j++;
-        //     else {
-        //         set.add(nums1[i]);
-        //         i++;
-        //         j++;
-        //     }
-        // }
-        for(int val : nums1){
-            if(binary(nums2, val))
-                set.add(val);
+        int i=0, j=0;
+        while(i<nums1.length && j<nums2.length) {
+            if(nums1[i] < nums2[j]) i++;
+            else if(nums1[i] > nums2[j]) j++;
+            else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
         }
+        // for(int val : nums1){
+        //     if(binary(nums2, val))
+        //         set.add(val);
+        // }
         int[] result = new int[set.size()];
         int k=0;
         for(int n : set) {
@@ -25,14 +25,14 @@ class Solution {
         return result;
     }
     
-    public static boolean binary(int[] nums, int k) {
-        int l=0, h=nums.length-1;
-        while(l<=h) {
-            int mid = l+(h-l)/2;
-            if(nums[mid] == k) return true;
-            if(nums[mid] < k) l=mid+1;
-            else h = mid-1;
-        }
-        return false;
-    }
+    // public static boolean binary(int[] nums, int k) {
+    //     int l=0, h=nums.length-1;
+    //     while(l<=h) {
+    //         int mid = l+(h-l)/2;
+    //         if(nums[mid] == k) return true;
+    //         if(nums[mid] < k) l=mid+1;
+    //         else h = mid-1;
+    //     }
+    //     return false;
+    // }
 }
