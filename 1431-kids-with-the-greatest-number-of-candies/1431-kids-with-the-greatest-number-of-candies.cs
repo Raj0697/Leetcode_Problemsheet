75bcdefd2a8b0efa0d4099cddf5d5439e9fd2829
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2023-04-17 10:55:02 +0530

    Time: 146 ms (85.11%), Space: 44 MB (97.09%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
+        int n = candies.Length;
+        bool[] result = new bool[n];
+        int max = candies.Max();
+        for(int i = 0; i < n; i++)
+        {
+            result[i] = candies[i] + extraCandies >= max ? true : false;
+        }
+        return result;
+    }
+}
