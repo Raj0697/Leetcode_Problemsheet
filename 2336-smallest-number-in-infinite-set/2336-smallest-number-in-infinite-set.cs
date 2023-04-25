7614ca8f211953e87de4d6531f60364a2b31ed0d
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2023-04-25 10:35:46 +0530

    Time: 179 ms (48.00%), Space: 57.7 MB (20.00%) - LeetHub

---
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cs
@@ -0,0 +1,26 @@
+public class SmallestInfiniteSet {
+
+    PriorityQueue<int, int> pq;
+    public SmallestInfiniteSet() {
+        pq = new PriorityQueue<int, int>(Enumerable.Range(1, 1000).Select(x => (x, x)));
+    }
+    
+    public int PopSmallest() {
+        int smallest = pq.Dequeue();
+        while (pq.Count > 0 && pq.Peek() == smallest)
+            pq.Dequeue();
+
+        return smallest;
+    }
+    
+    public void AddBack(int num) {
+        pq.Enqueue(num, num);
+    }
+}
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet obj = new SmallestInfiniteSet();
+ * int param_1 = obj.PopSmallest();
+ * obj.AddBack(num);
+ */
