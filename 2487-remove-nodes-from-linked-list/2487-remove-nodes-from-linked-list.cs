6d7a23fb834009e8dab96ae8c2094e2f16b13920
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2024-05-06 11:47:53 +0530

    Time: 444 ms (60.56%), Space: 103.5 MB (66.20%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cs
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveNodes(ListNode head) {
+        head = ReverseList(head);
+        int maximum = 0;
+        ListNode prev = null;
+        ListNode current = head;
+        
+        while (current != null) {
+            maximum = Math.Max(maximum, current.val);
+            if (current.val < maximum) {
+                ListNode deleted = current;
+                current = current.next;
+                if (prev != null) {
+                    prev.next = current;
+                }
+                deleted.next = null;
+            } else {
+                prev = current;
+                current = current.next;
+            }
+        }
+        
+        return ReverseList(head);
+    }
+    private ListNode ReverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode nextTemp = null;
+        
+        while (current != null) {
+            nextTemp = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextTemp;
+        }
+        
+        return prev;
+    }
+}
