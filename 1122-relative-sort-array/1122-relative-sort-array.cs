8890a2af985162b1c9dd5994ab92fde65cc23e63
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2024-06-11 11:48:07 +0530

    Time: 107 ms (47.73%), Space: 45.4 MB (92.05%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public int[] RelativeSortArray(int[] arr1, int[] arr2) {
+        var elementsInArr2 = new HashSet<int>();
+        foreach (var element in arr2)
+        {
+            elementsInArr2.Add(element);
+        }
+
+        var arr1ElementsNotInArr2 = new PriorityQueue<int, int>();
+        var elementCountOfArr1 = new Dictionary<int, int>();
+        foreach (var element in arr1)
+        {
+            if (!elementsInArr2.Contains(element))
+            {
+                arr1ElementsNotInArr2.Enqueue(element, element);
+            }
+            elementCountOfArr1[element] = elementCountOfArr1.GetValueOrDefault(element, 0) + 1;
+        }
+
+        var currentIndexArr1 = 0;
+        foreach (var element in arr2)
+        {
+            var elementCount = elementCountOfArr1[element];
+            for (int j = 0; j < elementCount; j++)
+            {
+                arr1[currentIndexArr1] = element;
+                currentIndexArr1 += 1;
+            }
+        }
+
+        while (arr1ElementsNotInArr2.Count > 0)
+        {
+            arr1[currentIndexArr1] = arr1ElementsNotInArr2.Dequeue();
+            currentIndexArr1 += 1;
+        }
+
+        return arr1;
+    }
+}
