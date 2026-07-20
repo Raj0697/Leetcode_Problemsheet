22f627172a2810a28352bf87ca36bbf393c8b3a8
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2026-07-20 15:00:02 +0530

    Time: 9 ms (96.18%), Space: 68.4 MB (38.76%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] DailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.Length];
+        Stack<int> stack = new();
+        for (int i = 0; i < temperatures.Length; i++)
+        {
+            while (stack.Count > 0 && temperatures[stack.Peek()] < temperatures[i])
+            {
+                int prevIndex = stack.Pop();
+                result[prevIndex] = i - prevIndex;
+            }
+
+            stack.Push(i);
+        }
+
+        return result;
+    }
+}
