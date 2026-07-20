bc4e662395c3a080cf66acb62a832351dc953866
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2026-07-20 13:11:25 +0530

    Time: 0 ms (100%), Space: 51.8 MB (62.67%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        int n = nums.Length;
+        int[] res = new int[2*n];
+        for(int i=0; i<n; i++){
+            res[i] = nums[i];
+            res[i+n] = nums[i];
+        }
+        return res;
+    }
+}
