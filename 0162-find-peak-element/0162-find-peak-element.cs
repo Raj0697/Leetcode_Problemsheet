aba8369ca0fa2dd73790479d262c86aacefdd2b5
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2026-07-20 14:59:42 +0530

    Time: 0 ms (100%), Space: 42.8 MB (29.55%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cs
@@ -0,0 +1,24 @@
+public class Solution 
+{
+    public int FindPeakElement(int[] nums)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left + 1 < right)
+        {
+            var mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[mid + 1])
+            {
+                left = mid;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+
+        return nums[left] > nums[right] ? left : right;
+    }
+}
