2ec77f350ecc73f890bc3fa41d61e5b0777ce3ad
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2026-07-21 16:27:49 +0530

    Time: 0 ms (100%), Space: 51.4 MB (45.64%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public TreeNode SearchBST(TreeNode root, int val) {
+        if(root == null || root.val == val)
+        {
+             return root;
+        }
+        
+        if(root.val > val)
+        {
+            return SearchBST(root.left, val);
+        }
+        else
+        {
+            return SearchBST(root.right, val);
+        }
+    }
+}
