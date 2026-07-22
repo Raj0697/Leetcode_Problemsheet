37c6c18fd8d6a2128c03bd224a59c7463cff1b95
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2026-07-22 11:44:02 +0530

    Time: 68 ms (87.76%), Space: 42.3 MB (89.41%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cs
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node next;
+    public Node random;
+    
+    public Node(int _val) {
+        val = _val;
+        next = null;
+        random = null;
+    }
+}
+*/
+
+public class Solution {
+    public Node CopyRandomList(Node head) {
+        if (head == null)
+            return null;
+         var tracking = new Dictionary<Node, Node>();
+        Node cur = head;
+        // Copy node without next and random
+        while (cur != null)
+        {
+            tracking.Add(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+
+        cur = head;
+        // Connect all node
+        while (cur != null)
+        {
+            tracking[cur].next = cur.next != null ? tracking[cur.next] : null;
+            tracking[cur].random = cur.random != null ? tracking[cur.random] : null;
+            cur = cur.next;
+        }
+
+        return tracking[head];
+    }
+}
