1379c54edac1ef209718945c04df71466656c036
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2026-07-22 13:42:17 +0530

    Time: 5 ms (77.15%), Space: 43.8 MB (94.5%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    private static Dictionary<string, Func<int, int, int>> s_Funcs = new () {
+      { "+", (a, b) => a + b },
+      { "-", (a, b) => b - a },
+      { "*", (a, b) => a * b },
+      { "/", (a, b) => b / a },   
+    };
+
+    public int EvalRPN(string[] tokens) {
+      Stack<int> data = new ();
+        
+      foreach (string token in tokens) 
+        if (int.TryParse(token, out int value))
+          data.Push(value);
+        else   
+          data.Push(s_Funcs[token](data.Pop(), data.Pop()));
+        
+      return data.Pop();
+    }
+}
