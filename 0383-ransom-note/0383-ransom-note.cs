9f051daf76f3abb4d0ac653fcb2c110b5a00f6fe
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2026-07-22 13:40:04 +0530

    Time: 6 ms (69.21%), Space: 48.1 MB (46.89%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool CanConstruct(string ransomNote, string magazine)
+        {
+            List<char> list = new List<char>(magazine);
+            for (int i = 0; i < ransomNote.Length; i++)
+            {
+                if (!list.Remove(ransomNote[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+}
