a7c3da591426f6cef038b9f29e0e8aa1c115dad8
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2026-07-26 13:12:24 +0530

    Time: 1 ms (100%), Space: 56.1 MB (75%) - LeetHub

---
+++ 0335-self-crossing/0335-self-crossing.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public bool IsSelfCrossing(int[] distance) {
+        int n = distance.Length;
+if (n < 4) return false;
+
+for (int i = 3; i < n; i++)
+{
+    // Case 1: current line crosses the line 3 steps ahead
+    if (distance[i] >= distance[i - 2] && distance[i - 1] <= distance[i - 3])
+    {
+        return true;
+    }
+
+    // Case 2: current line overlaps the line 4 steps ahead
+    if (i >= 4 && distance[i - 1] == distance[i - 3] &&
+        distance[i] + distance[i - 4] >= distance[i - 2])
+    {
+        return true;
+    }
+
+    // Case 3: current line crosses the line 5 steps ahead
+    if (i >= 5 &&
+        distance[i - 2] >= distance[i - 4] &&
+        distance[i] + distance[i - 4] >= distance[i - 2] &&
+        distance[i - 1] <= distance[i - 3] &&
+        distance[i - 1] + distance[i - 5] >= distance[i - 3])
+    {
+        return true;
+    }
+}
+
+return false;
+    }
+}
