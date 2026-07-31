1679309a34614c377345ad2aea07167d7a673078
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2026-07-31 13:38:04 +0530

    Time: 219 ms (21.74%), Space: 66.5 MB (15.22%) - LeetHub

---
+++ 0952-largest-component-size-by-common-factor/0952-largest-component-size-by-common-factor.cs
@@ -0,0 +1,93 @@
+public class Solution {
+        public int LargestComponentSize(int[] nums)
+        {
+            int maxValue = nums.Max();
+            var uf = new UnionFind(maxValue + 1);
+
+            var numFactorMap = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                var primeFactors = PrimeDecompose(num);
+
+                numFactorMap[num] = primeFactors[0];
+                for (int i = 0; i < primeFactors.Count - 1; ++i)
+                    uf.Union(primeFactors[i], primeFactors[i + 1]);
+            }
+
+            // count the size of group one by one
+            int maxGroupSize = 0;
+            var groupCount = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                var groupId = uf.Find(numFactorMap[num]);
+                groupCount.TryGetValue(groupId, out int count);
+                groupCount[groupId] = count + 1;
+
+                maxGroupSize = Math.Max(maxGroupSize, count + 1);
+            }
+
+            return maxGroupSize;
+        }
+
+        private IList<int> PrimeDecompose(int num)
+        {
+            var primeFactors = new List<int>();
+
+            int factor = 2;
+            while (num >= factor * factor)
+            {
+                if (num % factor == 0)
+                {
+                    primeFactors.Add(factor);
+                    num /= factor;
+                }
+                else
+                    factor += 1;
+            }
+
+            primeFactors.Add(num);
+
+            return primeFactors.Distinct().ToList();
+        }
+
+        public class UnionFind
+        {
+            private int[] parents;
+            private int[] ranks;
+
+            public UnionFind(int count)
+            {
+                parents = new int[count];
+                ranks = new int[count];
+
+                for (int i = 0; i < count; i++)
+                    parents[i] = i;
+            }
+
+            public int Find(int index)
+            {
+                if (parents[index] != index)
+                    parents[index] = Find(parents[index]);
+
+                return parents[index];
+            }
+
+            public void Union(int index1, int index2)
+            {
+                var pIndex1 = Find(index1);
+                var pIndex2 = Find(index2);
+
+                if (pIndex1 == pIndex2) return;
+                if (ranks[pIndex1] >= ranks[pIndex2])
+                {
+                    parents[pIndex2] = pIndex1;
+                    ranks[pIndex2]++;
+                }
+                else
+                {
+                    parents[pIndex1] = pIndex2;
+                    ranks[pIndex1]++;
+                }
+            }
+        }
+}
