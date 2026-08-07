cb98e9f6699d34033474fe644f5054aaa5bfadba
RAJKUMAR M <50259920+Raj0697@users.noreply.github.com>
2026-08-07 12:15:23 +0530

    Time: 0 ms (100%), Space: 41.3 MB (97.76%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cs
@@ -0,0 +1,31 @@
+public class Solution {
+       public  int MaxProfit(int[] prices)
+        {
+            int n = prices.Length;
+            if (n == 0 || n == 1)
+            {
+                return 0;
+            }
+
+            int[] sell = new int[n];
+            int[] buy = new int[n];
+
+            buy[0] = -prices[0];
+            sell[0] = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                sell[i] = Math.Max(sell[i - 1], buy[i - 1] + prices[i]);
+                if (i == 1)
+                {
+                    buy[i] = Math.Max(buy[i - 1], -prices[i]);
+                }
+                else
+                {
+                    buy[i] = Math.Max(buy[i - 1], sell[i - 2] - prices[i]);
+                }
+            }
+
+            return sell[n - 1];
+        }
+    }
